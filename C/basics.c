44a2b85a5adf60924037056ad00cc55d50627898
Max Taylor <2027Taylor.John@fcboe.org>
2024-01-22 15:41:43 -0500

    Finished basics

---
+++ C/basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("This is my c basics program.\n");
+	int num;
+	num = 4459-4039;
+	printf("Give me a number\n");
+	scanf("%i", &num);
+	printf("You entered %i.\n", num);
+
+	return 0;
+}
